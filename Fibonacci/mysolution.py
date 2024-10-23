b45ce389464ac10dbe871f6327bd4769f360fc89
sudesh.tnbt <sudeshacharya110@gmail.com>
2024-10-23 13:52:56 +0545

    My recursive approach to the problem.

--- Fibonacci/mysolution.py
+++ Fibonacci/mysolution.py
@@ -1,23 +1,41 @@
 def fibonacci1(n):
     # Manual approach
-    n = int(n)
-    a = 1
+    a = 0
     b = 1
-    fibo = []
+    result = []
     if n > 2:
-        fibo.append(a)
-        fibo.append(b)
+        result.append(a)
+        result.append(b)
         for i in range(2, n):
             if i % 2 == 0:
                 a = a + b
-                fibo.append(a)
+                result.append(a)
             else:
                 b = a + b
-                fibo.append(b)
-    return fibo
+                result.append(b)
+    return result
+
+
+def fibo(n):
+    # Recursive function
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibo(n - 1) + fibo(n - 2)
+
+
+def fibonacci2(n):
+    # Recursive approach
+    result = []
+    for i in range(n):
+        result.append(fibo(i))
+    return result
 
 
 # Asks the user for the number of terms
-n = input("Enter no. of terms: ")
-result = fibonacci1(n)
-print(result)
+n = int(input("Enter no. of terms: "))
+result1 = fibonacci1(n)
+result2 = fibonacci2(n)
+print(result1)
+print(result2)
